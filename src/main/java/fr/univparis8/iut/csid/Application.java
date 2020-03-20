cf18d3a1c26989eb946546bef65974c92c6d418e
Alexandre Thebault <37109807+holyris@users.noreply.github.com>
2020-03-20 15:50:01 +0100

    First commit

---
+++ src/main/java/fr/univparis8/iut/csid/Application.java
@@ -0,0 +1,13 @@
+package fr.univparis8.iut.csid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
---
+++ src/main/java/fr/univparis8/iut/csid/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package fr.univparis8.iut.csid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  public DataSource dataSource;
+
+  protected void configure(HttpSecurity http) throws Exception {
+    http.formLogin().disable().authorizeRequests()
+            .antMatchers("/").permitAll()
+            .antMatchers("/h2-console/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .csrf().disable();
+    http.headers().frameOptions().sameOrigin();
+
+  }
+}
