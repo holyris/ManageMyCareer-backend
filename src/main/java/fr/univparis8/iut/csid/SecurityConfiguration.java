ef77e21e04cba47c71f252a89978ad0c0042c3ca
Alexandre Thebault <alexandre.thebault77@gmail.com>
2020-04-02 16:29:38 +0200

    Fix cors

--- src/main/java/fr/univparis8/iut/csid/SecurityConfiguration.java
+++ src/main/java/fr/univparis8/iut/csid/SecurityConfiguration.java
@@ -1,18 +1,12 @@
 package fr.univparis8.iut.csid;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -21,7 +15,6 @@ import javax.sql.DataSource;
 import java.util.Arrays;
 
 @EnableWebSecurity
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   public final DataSource dataSource;
@@ -40,29 +33,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .csrf().disable();
     http.headers().frameOptions().sameOrigin();
     http.cors().configurationSource(corsConfigurationSource());
-
-//    http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
-
   }
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.jdbcAuthentication().dataSource(dataSource)
-            .withUser(
-                    User.withUsername("admin")
-                            .password(passwordEncoder().encode("admin"))
-                            .authorities(AuthorityUtils.createAuthorityList("ADMIN")).build()
-            );
-/*
-            .usersByUsernameQuery(
-                    "select username,password, enabled from users where username=?")
-            .authoritiesByUsernameQuery(
-                    "select username, authority from authorities where username=?")
-            .passwordEncoder(new BCryptPasswordEncoder());
-*/
-
-    auth.userDetailsService(jdbcUserDetailsManager());
-  }
+//  @Override
+//  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//    auth.jdbcAuthentication().dataSource(dataSource)
+//            .withUser(
+//                    User.withUsername("admin")
+//                            .password(passwordEncoder().encode("admin"))
+//                            .authorities(AuthorityUtils.createAuthorityList("ADMIN")).build()
+//            );
+///*
+//            .usersByUsernameQuery(
+//                    "select username,password, enabled from users where username=?")
+//            .authoritiesByUsernameQuery(
+//                    "select username, authority from authorities where username=?")
+//            .passwordEncoder(new BCryptPasswordEncoder());
+//*/
+//
+//    auth.userDetailsService(jdbcUserDetailsManager());
+//  }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
@@ -70,7 +60,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   }
 
   @Bean
-  public JdbcUserDetailsManager jdbcUserDetailsManager(){
+  public JdbcUserDetailsManager jdbcUserDetailsManager() {
     JdbcUserDetailsManager userDetailsService = new JdbcUserDetailsManager();
     userDetailsService.setDataSource(dataSource);
     return userDetailsService;
