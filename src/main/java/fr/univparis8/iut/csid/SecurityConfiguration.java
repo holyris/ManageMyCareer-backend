b42ff5d344d20726d7d64f19a793fbf982d3f312
Yann <yann.hippler@gmail.com>
2020-04-02 16:06:26 +0200

    fix cors

--- src/main/java/fr/univparis8/iut/csid/SecurityConfiguration.java
+++ src/main/java/fr/univparis8/iut/csid/SecurityConfiguration.java
@@ -1,22 +1,36 @@
 package fr.univparis8.iut.csid;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
 
 @EnableWebSecurity
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-  @Autowired
-  public DataSource dataSource;
+  public final DataSource dataSource;
 
+  public SecurityConfiguration(DataSource dataSource){
+    this.dataSource = dataSource;
+  }
+
+  @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.formLogin().disable().authorizeRequests()
             .antMatchers("/**").permitAll()
@@ -25,8 +39,51 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .and()
             .csrf().disable();
     http.headers().frameOptions().sameOrigin();
-    http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+    http.cors().configurationSource(corsConfigurationSource());
+
+//    http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
 
   }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.jdbcAuthentication().dataSource(dataSource)
+            .withUser(
+                    User.withUsername("admin")
+                            .password(passwordEncoder().encode("admin"))
+                            .authorities(AuthorityUtils.createAuthorityList("ADMIN")).build()
+            );
+/*
+            .usersByUsernameQuery(
+                    "select username,password, enabled from users where username=?")
+            .authoritiesByUsernameQuery(
+                    "select username, authority from authorities where username=?")
+            .passwordEncoder(new BCryptPasswordEncoder());
+*/
+
+    auth.userDetailsService(jdbcUserDetailsManager());
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public JdbcUserDetailsManager jdbcUserDetailsManager(){
+    JdbcUserDetailsManager userDetailsService = new JdbcUserDetailsManager();
+    userDetailsService.setDataSource(dataSource);
+    return userDetailsService;
+  }
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("*"));
+    configuration.setAllowedMethods(Arrays.asList("GET","POST", "DELETE", "PUT", "PATCH", "OPTIONS"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+
 
 }
