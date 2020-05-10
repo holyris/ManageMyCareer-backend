e7cd6e01c649c86c4c55d68089c10bbd06524110
Alexandre Thebault <alexandre@acta-ds.fr>
2020-05-10 20:00:56 +0200

    Changement endpoint /companies

--- src/main/java/fr/univparis8/iut/csid/company/CompanyController.java
+++ src/main/java/fr/univparis8/iut/csid/company/CompanyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("company")
+@RequestMapping("companies")
 public class CompanyController {
 
     @Autowired
